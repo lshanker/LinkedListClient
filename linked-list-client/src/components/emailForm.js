5eb863ed5b5c1cf5381a03fe7f38828713da45e5
Noah <john1931@purdue.edu>
2018-04-14 23:18:11 -0400

    Continuation of email form

--- linked-list-client/src/components/emailForm.js
+++ linked-list-client/src/components/emailForm.js
@@ -1,1 +1,33 @@
 import React, { Component } from 'react';
+
+const INITIAL_STATE = {
+    subject: '',
+    message: '',
+    error: null,
+  };
+
+
+class emailForm extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={...INITIAL_STATE};
+    }
+    render(){
+        return (
+        
+            <form onSubmit={this.onSubmit}>
+                <p className="h5 text-center mb-4">Write to us</p>
+                <Input label="Your name" icon="user" group type="email" validate error="wrong" success="right"/>
+                <Input label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+                <Input label="Subject" icon="tag" group type="email" validate error="wrong" success="right"/>
+                <Input type="textarea" label="Your message" icon="pencil"/>
+                <div className="text-center">
+                    <Button color="deep-orange" color="unique">Send <Fa icon="paper-plane-o" className="ml-1"/></Button>
+                </div>
+            </form>
+
+        )
+    }   
+
+}
