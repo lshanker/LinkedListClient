b4f0bd6a365585beb05e2225170cdbb77d625c3e
Noah <john1931@purdue.edu>
2018-04-15 23:48:01 -0400

    Added styling, just needs to be able to store the messages now

--- linked-list-client/src/components/emailForm.js
+++ linked-list-client/src/components/emailForm.js
@@ -21,27 +21,50 @@ class EmailForm extends Component{
         this.state={...INITIAL_STATE};
     }
 
+    onSubmit= (event) => {
+        // event.preventDefault;
+
+        const {
+            subject,
+            message,
+        } = this.state;
+
+
+    }
+
     render(){
 
         const {
             subject,
             message,
-            error,
+            // error,
           } = this.state;
 
         return (
         
             <form onSubmit={this.onSubmit}>
-                <p className="h5 text-center mb-4">Email this List!</p>
-                <input placeholder="Subject" 
-                    type="text" 
-                    value={subject}     
-                    onChange={event => this.setState(byPropKey('subject', event.target.value))}
-                />
-                <input placeholder="Enter your message here" type="text" value={message} onChange={event => this.setState(byPropKey('message', event.target.value))}/>
-                <div className="text-center">
-                    <button className="btn #4a148c purple darken-4">Send</button>
-                    <button className="btn #ff5722 deep-orange">Discard</button>
+                <p className="h5 text-center mb-4 mt-4">Email this List!</p>
+                <div className="md-form">
+                    <input
+                        className="mt-3" 
+                        placeholder="Subject" 
+                        type="text" 
+                        value={subject}     
+                        onChange={event => this.setState(byPropKey('subject', event.target.value))}
+                    />
+                    <textarea
+                        className="form-control mt-3 md-textarea width:100%"
+                        rows="3" 
+                        placeholder="Enter your message here" 
+                        type="text" 
+                        value={message} 
+                        onChange={event => this.setState(byPropKey('message', event.target.value))}
+                    />
+                
+                    <div className="text-center">
+                        <button className="btn #4a148c purple darken-4">Send</button>
+                        <button className="btn #ff5722 deep-orange">Discard</button>
+                    </div>
                 </div>
             </form>
         )
